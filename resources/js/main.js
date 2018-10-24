f46f962bf1a970d7d37bd2238f6b5e5fd65f3287
Sammii-HK <sammii.h@icloud.com>
2018-10-24 23:43:15 +0100

    touch enabled browser hover fix js insert

--- resources/js/main.js
+++ resources/js/main.js
@@ -31,5 +31,9 @@ $(document).ready(function() {
     }
   });
 	
+  $('.hover').on('touchstart touchend', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('hover_effect');
+    });
 
 });
