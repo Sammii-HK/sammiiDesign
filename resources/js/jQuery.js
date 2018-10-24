22194fd1ff9b2051b1f190f5200e7aebe4ab6889
Sammii-HK <sammii.h@icloud.com>
2018-10-24 23:48:07 +0100

    jquery hover portfolio page fix insert

---
+++ resources/js/jQuery.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+$(document).ready(function() {
+    $('.hover').on('touchstart touchend', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('hover_effect');
+    });
+});
