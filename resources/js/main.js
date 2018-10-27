46ff3caccf96e22839fd4f4a30d9a9351ae3bf00
Sammii-HK <sammii.h@icloud.com>
2018-10-27 21:42:05 +0100

    mob touch bug

--- resources/js/main.js
+++ resources/js/main.js
@@ -33,9 +33,9 @@ $(document).ready(function() {
     }
   });
 	
-  $('.hover').on('touchstart touchend', function(e) {
-        e.preventDefault();
-        $(this).toggleClass('hover_effect');
-    });
+  //$('.hover').on('touchstart touchend', function(e) {
+  //      e.preventDefault();
+  //      $(this).toggleClass('hover_effect');
+  //  });
 
 });
